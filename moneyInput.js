9c52932085988e5ba7436664f01f3dab4ba8855c
Victor <victor.prado@capco.com>
2022-08-09 01:51:08 -0300

    fix currency mask

--- moneyInput.js
+++ moneyInput.js
@@ -1,22 +1,11 @@
-const padWithZeros = (value) => {
-  let s = String(value);
-  let needLeadingZeros = s.length <= 3;
-  if (needLeadingZeros) {
-    while (s.length <= 3) {
-      s = "0" + s;
-    }
-  } else {
-    s = s.replace(/^0{0,2}/g, '');
-  }
-  return s;
-}
-
 const applyMask = ({ target }) => {
   const value = target.value;
-  const padded = padWithZeros(value);
-  const masked = padded.replace(/\D/g, '')
+  const masked = value
+	  .replace(/\D/g, '')
+	  .padStart(4, '0')
     .replace(/(\d)(\d{2})$/g, '$1,$2')
-    .replace(/(?=(\d{3})+(\D))\B/g, '.');
+    .replace(/(?=(\d{3})+(\D))\B/g, '.')
+	  .replace(/^0?/, '');
   target.value = masked;
   const currency = parseFloat(masked.replace(/\./g, '').replace(/,/g, '.'));
   console.log(currency);
